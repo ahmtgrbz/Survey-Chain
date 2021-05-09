07b31c01a7fac9705685559aecece20d3bd5f714
ahmtgrbz <ahmetgurbuz2@posta.mu.edu.tr>
2021-05-10 01:45:36 +0300

    Getter Functions and Comments Added
    Getter Functions and Comments Added

--- main.sol
+++ main.sol
@@ -1,16 +1,23 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.1;
+
+
 contract SurveyList {
     
+    // These are for our models ids.
     uint public surveyCount = 0;
     uint public questionCount = 0;
     
     
-    
+    // Survay Struct model
     struct Survey{
         uint id;
+        string title;
         Question question;
     }
     
+    // Question Struct model
     struct Question{
         uint id;
         bool completed;
@@ -19,34 +26,39 @@ contract SurveyList {
         string selectedAnswer;
     }
     
+    //------------events for creating question and survey------------ .
     event QuestionCreated(uint id, bool completed, string _content, string answer, string selectedAnswer);
-    event SurveyCreated(uint id, Question question);
+    event SurveyCreated(uint id, string title, Question question);
     
+    //Creating mapping structure to keep data for question and survey.
     mapping(uint => Question) public questions;
-
     mapping(uint => Survey) public surveys;
 
-    
+    //------------Creating functions------------
     function createQuestion(string memory _content, string memory answer, string memory selectedAnswer) public {
         questions[questionCount] = Question(questionCount, false, _content, answer, selectedAnswer);
         emit QuestionCreated(questionCount, false,_content, answer, selectedAnswer);
         questionCount++;
     }
     
-    function createSurvey(uint id) public {
+    function createSurvey(uint id, string memory title) public {
         Question memory question = questions[id];
-        surveys[surveyCount] = Survey(surveyCount, question);
-        emit SurveyCreated(surveyCount, question);
+        surveys[surveyCount] = Survey(surveyCount,title, question);
+        emit SurveyCreated(surveyCount, title, question);
         surveyCount++;
-
-        
     }
+    
+    
+    //------------Getter functions------------
+      function getSurvay(uint id) public view returns (string memory){
+        Survey memory survey = surveys[id];
+        return (survey.title);
+    }
+    
     
     function getQuestion(uint id) public view returns (string memory){
         Question memory question = questions[id];
         return (question.answer);
     }
-    
-    
     
 }
