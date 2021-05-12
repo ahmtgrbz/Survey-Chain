16afd6c2723fa58c7d6b1d2456e2ab4ce17a97ce
Onur Duman <onurdumn99@gmail.com>
2021-05-12 13:43:54 +0300

    Participant model added

--- main.sol
+++ main.sol
@@ -1,4 +1,3 @@
-
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.1;
 
@@ -8,6 +7,7 @@ contract SurveyList {
     // These are for our models ids.
     uint public surveyCount = 0;
     uint public questionCount = 0;
+    uint public ParticipantCount = 0;
     
     
     // Survay Struct model
@@ -26,13 +26,25 @@ contract SurveyList {
         string selectedAnswer;
     }
     
+    struct Participant{
+        address p_address;
+        string name;
+        uint age;
+        uint joined_surveys;
+    }
+    
+    address[] participant_list;
+    
     //------------events for creating question and survey------------ .
     event QuestionCreated(uint id, bool completed, string _content, string answer, string selectedAnswer);
     event SurveyCreated(uint id, string title, Question question);
+    event ParticipantCreated(address p_address, string name, uint age, uint joined_surveys);
+
     
     //Creating mapping structure to keep data for question and survey.
     mapping(uint => Question) public questions;
     mapping(uint => Survey) public surveys;
+    mapping(address => Participant) public participants;
 
     //------------Creating functions------------
     function createQuestion(string memory _content, string memory answer, string memory selectedAnswer) public {
@@ -48,9 +60,16 @@ contract SurveyList {
         surveyCount++;
     }
     
+    function createParticipant(string memory name, uint age) public{
+        
+        participants[address(this)] = Participant(address(this), name, age, ParticipantCount);
+        emit ParticipantCreated(address(this), name, age, ParticipantCount);
+        participant_list.push(address(this));
+    }
+    
     
     //------------Getter functions------------
-      function getSurvay(uint id) public view returns (string memory){
+      function getSurvey(uint id) public view returns (string memory){
         Survey memory survey = surveys[id];
         return (survey.title);
     }
@@ -60,5 +79,9 @@ contract SurveyList {
         Question memory question = questions[id];
         return (question.answer);
     }
+    
+    function getParticipantCount() public view returns (uint c){
+        return participant_list.length;
+    } 
     
 }
