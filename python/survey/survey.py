995247bd85f0e20697c6b7741262f6b075ef4ae3
Onur Duman <onurdumn99@gmail.com>
2021-06-07 23:41:50 +0300

    survey problem fixed

--- python/survey/survey.py
+++ python/survey/survey.py
@@ -2,8 +2,8 @@ class Survey:
     def __init__(self, web3, contract):
         self.web3 = web3
         self.contract = contract
-        self.question_count = self.contract.functions.getQuestionCount.call()
+        self.question_count = self.contract.functions.getQuestionCount().call()
 
 
     def getSurveyCount(self):
-        print(self.contract.functions.getSurveyCount.call())
+        print(self.contract.functions.getSurveyCount().call())
